b643d51ba606946dabdcda0d2ba5584143db5568
bhavishya-06 <bhavishyagarg2000@gmail.com>
2025-02-19 06:14:29 +0530

    m
    m

--- src/components/FullscreenMessage.tsx
+++
@@ -1,8 +0,0 @@
-const FullscreenMessage = () => {
-  return (
-    <div className="fullscreen-message">
-      <p>This quiz requires fullscreen mode</p>
-      <button onClick={/* your handler */}>Enter Fullscreen</button>
-    </div>
-  );
-}; 
